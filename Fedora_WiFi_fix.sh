5987a09a6f9ef05cee814bf35a3340336f288774
tfpf <vpaijc@gmail.com>
2022-01-20 18:17:06 +0530

    Remove blank lines; Add LM Realtek driver install command

--- Fedora_WiFi_fix.sh
+++ Fedora_WiFi_fix.sh
@@ -2,15 +2,10 @@
 
 # Broadcom WiFi adapter
 # 02:00.0 Network controller: Broadcom Limited BCM43142 802.11b/g/n (rev 01)
-
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-28.noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-28.noarch.rpm
-
 sudo dnf install -y broadcom-wl kernel-devel
 uname -r
 sudo dnf install kernel-devel-$(uname -r)
 sudo akmods --force --kernel $(uname -r) --akmod wl
-
-# Activate WiFi driver.
 sudo modprobe -a wl
-
--- Linux_Mint_WiFi_fix.sh
+++ Linux_Mint_WiFi_fix.sh
@@ -2,6 +2,7 @@
 
 # Broadcom WiFi adapter
 # 02:00.0 Network controller: Broadcom Limited BCM43142 802.11b/g/n (rev 01)
-
 sudo apt install bcmwl-kernel-source
 
+# Realtek RTL8821CE 802.11ac PCIe Wireless Network Adapter
+sudo apt install rtl8821ce-dkms
